55e97fd04244529b39250e385bf4f0e924b6de4b
aaae1324@naver.com <sds531568>
2020-12-31 11:46:00 +0900

    STS에서 수정 후  SourceTree 커밋

---
+++ src/GitTest.java
@@ -0,0 +1,11 @@
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("STS에서 GIT 사용 테스트1");
+		System.out.println("STS에서 GIT 사용 테스트2");
+		System.out.println("STS에서 SourceTree 테스트1");
+	}
+
+}
