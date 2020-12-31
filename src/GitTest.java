28f0913753dcac495402d87c4a3072b71ddef67a
aaae1324@naver.com <sds531568>
2020-12-31 12:29:36 +0900

    Git에서 다운받아 설치한 파일

--- src/GitTest.java
+++ src/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
 		System.out.println("STS에서 GIT 사용 테스트1");
 		System.out.println("STS에서 GIT 사용 테스트2");
 		System.out.println("STS에서 SourceTree 테스트1");
+		System.out.println("git에서 다운받아 설치한 파일");
 	}
 
 }
